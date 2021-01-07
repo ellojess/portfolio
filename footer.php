c0aa4f896bd7dfdbd4c46420a12d3623ec6c7951
Jess <jtrinh619@gmail.com>
2021-01-06 18:51:33 -0800

    add project content

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <p class="mb-1">&copy; Copyright Jessica Trinh. All Rights Reserved</p>
+          <div class="credits">
+          <a href="https://bootstrapmade.com/"></a>
+          </div>
+        </div>
+        <div class="col-sm-6 social text-md-right">
+          <a href="https://github.com/ellojess"><span class="icofont-toy-cat"></span></a>
+          <a href="https://www.linkedin.com/in/j-trinh/"><span class="icofont-linkedin"></span></a>
+          <a href="https://twitter.com/ellojesss"><span class="icofont-twitter"></span></a>
+          <a href="https://www.instagram.com/ellojess/?hl=en"><span class="icofont-instagram"></span></a>
+          <a href="https://www.facebook.com/ijessicatrinh/"><span class="icofont-facebook"></span></a>
+        </div>
+      </div>
+    </div>
+  </footer>
